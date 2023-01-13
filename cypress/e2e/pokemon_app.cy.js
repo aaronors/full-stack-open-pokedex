ddb0e25a60798ecb30da6b31495008a4622bc85f
Aaron Salazar <aaronors@outlook.com>
2023-01-13 14:39:52 -0800

    part11 - ex11.9 - add additional cy test

--- cypress/e2e/pokemon_app.cy.js
+++ cypress/e2e/pokemon_app.cy.js
@@ -4,4 +4,12 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  describe('when the front page has been opened', function() {
+    it('a pokemon link can be clicked', function() {
+      cy.visit('http://localhost:5000')
+      cy.contains('ivysaur').click()
+      cy.contains('chlorophyll')
+    })
+  })
 })
