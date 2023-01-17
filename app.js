57eb22cb75f418992c793a05d87bfd97f36fab2a
Aaron Salazar <aaronors@outlook.com>
2023-01-17 15:34:30 -0800

    part11 - ex11.11 - add http check

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+app.use(express.json())
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
 app.listen(PORT, () => {
   console.log('server started on port 5000')
