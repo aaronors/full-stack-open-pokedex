9898bbd4accd2b11999e02f3c4f7d36779dde776
Aaron Salazar <aaronors@outlook.com>
2023-01-17 16:24:26 -0800

    part11 - ex11.11 - change all port references to 8080

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 app.use(express.json())
@@ -16,5 +16,5 @@ app.get('/version', (req, res) => {
 })
 
 app.listen(PORT, () => {
-  console.log('server started on port 5000')
+  console.log('server started on port 8080')
 })
--- cypress/e2e/pokemon_app.cy.js
+++ cypress/e2e/pokemon_app.cy.js
@@ -1,13 +1,13 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
   describe('when the front page has been opened', function() {
     it('a pokemon link can be clicked', function() {
-      cy.visit('http://localhost:5000')
+      cy.visit('http://localhost:8080')
       cy.contains('ivysaur').click()
       cy.contains('chlorophyll')
     })
